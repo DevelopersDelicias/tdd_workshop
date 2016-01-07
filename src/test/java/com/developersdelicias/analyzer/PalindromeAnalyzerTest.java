a3f8a17bbcd2ce48a0e9055ff8a36265282ca02a
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-07 17:35:22 -0600

    Add test that fails (even compilation)

---
+++ src/test/java/com/developersdelicias/analyzer/PalindromeAnalyzerTest.java
@@ -0,0 +1,16 @@
+package com.developersdelicias.analyzer;
+
+import org.junit.Test;
+
+/**
+ * Description of the class PalindromeAnalyzerTest
+ * @author Benjamin Cisneros
+ */
+public class PalindromeAnalyzerTest {
+    
+    @Test
+    public void testAnalyze() {
+        PalindromeAnalyzer analyzer = new PalindromeAnalyzer();
+        assertTrue(analyzer.analyze("A man, a plan, a canal, Panama!"));
+    }
+}
