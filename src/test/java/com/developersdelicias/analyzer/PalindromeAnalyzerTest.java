c788b907f980b6473d6c53c93a487c64d665f92b
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-07 17:50:10 -0600

    Add new test cases

--- src/test/java/com/developersdelicias/analyzer/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/analyzer/PalindromeAnalyzerTest.java
@@ -14,6 +14,16 @@ public class PalindromeAnalyzerTest {
     public void testAnalyze() {
         PalindromeAnalyzer analyzer = new PalindromeAnalyzer();
         assertTrue(analyzer.analyze("A man, a plan, a canal, Panama!"));
+        assertTrue(analyzer.analyze("Amor, Roma"));
+        assertTrue(analyzer.analyze("Stack cats"));
+        assertTrue(analyzer.analyze("Step on no pets"));
+        assertTrue(analyzer.analyze("taco cat"));
+        assertTrue(analyzer.analyze("Put it up"));
+        assertTrue(analyzer.analyze("Anita lava la tina"));
+        assertTrue(analyzer.analyze("Was it a car or a cat I saw?"));
+        assertTrue(analyzer.analyze("No 'x' in Nixon"));
         assertFalse(analyzer.analyze("This string is not a palyndrome"));
+        assertFalse(analyzer.analyze("Hello World"));
+        assertFalse(analyzer.analyze("Anything else and more"));
     }
 }
