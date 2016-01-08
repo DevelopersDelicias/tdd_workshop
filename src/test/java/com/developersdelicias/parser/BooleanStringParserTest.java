c8e9d71fee8fb892f0bdf2a9ac8e84b947050283
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-07 19:52:21 -0600

    Complete all tests

--- src/test/java/com/developersdelicias/parser/BooleanStringParserTest.java
+++ src/test/java/com/developersdelicias/parser/BooleanStringParserTest.java
@@ -15,9 +15,14 @@ public abstract class BooleanStringParserTest {
     public void testParseTrueValues() {
         assertEquals(Boolean.TRUE, parser.parse("yes"));
         assertEquals(Boolean.TRUE, parser.parse("YES"));
+        assertEquals(Boolean.TRUE, parser.parse("Yes"));
         assertEquals(Boolean.TRUE, parser.parse("y"));
+        assertEquals(Boolean.TRUE, parser.parse("Y"));
         assertEquals(Boolean.TRUE, parser.parse("true"));
+        assertEquals(Boolean.TRUE, parser.parse("TRUE"));
+        assertEquals(Boolean.TRUE, parser.parse("True"));
         assertEquals(Boolean.TRUE, parser.parse("t"));
+        assertEquals(Boolean.TRUE, parser.parse("T"));
         assertEquals(Boolean.TRUE, parser.parse("1"));
         assertEquals(Boolean.TRUE, parser.parse("2"));
         assertEquals(Boolean.TRUE, parser.parse("3"));
@@ -26,9 +31,15 @@ public abstract class BooleanStringParserTest {
     @Test
     public void testParseFalseValues() {
         assertEquals(Boolean.FALSE, parser.parse("no"));
+        assertEquals(Boolean.FALSE, parser.parse("NO"));
+        assertEquals(Boolean.FALSE, parser.parse("No"));
         assertEquals(Boolean.FALSE, parser.parse("n"));
+        assertEquals(Boolean.FALSE, parser.parse("N"));
         assertEquals(Boolean.FALSE, parser.parse("false"));
+        assertEquals(Boolean.FALSE, parser.parse("FALSE"));
+        assertEquals(Boolean.FALSE, parser.parse("False"));
         assertEquals(Boolean.FALSE, parser.parse("f"));
+        assertEquals(Boolean.FALSE, parser.parse("F"));
         assertEquals(Boolean.FALSE, parser.parse("0"));
         assertEquals(Boolean.FALSE, parser.parse("-1"));
     }
