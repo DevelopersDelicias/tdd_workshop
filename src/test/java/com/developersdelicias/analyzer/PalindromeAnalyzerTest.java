dbab8deb731f5402caf5f230e975d10e39e52113
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-07 18:38:47 -0600

    Add parametrized test for real palindromes

--- src/test/java/com/developersdelicias/analyzer/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/analyzer/PalindromeAnalyzerTest.java
@@ -1,28 +1,27 @@
 package com.developersdelicias.analyzer;
 
+import java.util.ArrayList;
+import java.util.List;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 /**
  * Description of the class PalindromeAnalyzerTest
  * @author Benjamin Cisneros
  */
+@RunWith(JUnitParamsRunner.class)
 public class PalindromeAnalyzerTest {
     private final PalindromeAnalyzer analyzer = new PalindromeAnalyzer();
     
     @Test
-    public void testAnalyzeRealPalindromes() {
-        assertTrue(analyzer.analyze("A man, a plan, a canal, Panama!"));
-        assertTrue(analyzer.analyze("Amor, Roma"));
-        assertTrue(analyzer.analyze("Stack cats"));
-        assertTrue(analyzer.analyze("Step on no pets"));
-        assertTrue(analyzer.analyze("taco cat"));
-        assertTrue(analyzer.analyze("Put it up"));
-        assertTrue(analyzer.analyze("Anita lava la tina"));
-        assertTrue(analyzer.analyze("Was it a car or a cat I saw?"));
-        assertTrue(analyzer.analyze("No 'x' in Nixon"));
-        assertTrue(analyzer.analyze("123321"));
+    @Parameters(method = "getRealPalindromes")
+    public void testAnalyzeRealPalindromes(String phrase) {
+        assertTrue(analyzer.analyze(phrase));
+        
     }
     
     @Test 
@@ -35,5 +34,22 @@ public class PalindromeAnalyzerTest {
         assertFalse(analyzer.analyze("          "));
         assertFalse(analyzer.analyze("!#$%&()=?¡¿"));
         assertFalse(analyzer.analyze("1234567890"));
+    }
+    
+    public static List<String> getRealPalindromes() {
+        List<String> realPalindromes = new ArrayList<>();
+        realPalindromes.add("A man, a plan, a canal, Panama!");
+        realPalindromes.add("Amor, Roma");
+        realPalindromes.add("Race car");
+        realPalindromes.add("Stack cats");
+        realPalindromes.add("Step on no pets");
+        realPalindromes.add("taco cat");
+        realPalindromes.add("Put it up");
+        realPalindromes.add("Anita lava la tina");
+        realPalindromes.add("Was it a car or a cat I saw?");
+        realPalindromes.add("No 'x' in Nixon");
+        realPalindromes.add("123321");
+
+        return realPalindromes;
     }
 }
