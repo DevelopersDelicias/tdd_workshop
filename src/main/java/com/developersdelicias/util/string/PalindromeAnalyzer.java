f85d4bd07774c118f352838427c007da7dfc1fec
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-08 15:20:37 -0600

    Add classes to compile the class but we got a NullPointerException

---
+++ src/main/java/com/developersdelicias/util/string/PalindromeAnalyzer.java
@@ -0,0 +1,11 @@
+package com.developersdelicias.util.string;
+
+/**
+ * Define operations related with palindromes
+ *
+ * @author Benjamin Cisneros <benjamin.cisneros@softtek.com>
+ */
+public interface PalindromeAnalyzer {
+
+    public boolean isPalindrome(String phrase);
+}
--- src/test/java/com/developersdelicias/util/string/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/util/string/PalindromeAnalyzerTest.java
@@ -1,5 +1,6 @@
 package com.developersdelicias.util.string;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
