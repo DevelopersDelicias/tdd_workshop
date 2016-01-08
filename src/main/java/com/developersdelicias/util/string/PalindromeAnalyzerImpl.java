2babecd4022d65d52ea5ddbf42246214dbd8c356
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-08 15:26:24 -0600

    Adding the simplest code to pass isPalindromeWithSinglePalindromeMustReturnTrue test

---
+++ src/main/java/com/developersdelicias/util/string/PalindromeAnalyzerImpl.java
@@ -0,0 +1,14 @@
+package com.developersdelicias.util.string;
+
+/**
+ * Implementation class for PalindromeAnalyzer
+ * @author Benjamin Cisneros <benjamin.cisneros@softtek.com>
+ */
+public class PalindromeAnalyzerImpl implements PalindromeAnalyzer {
+
+    @Override
+    public boolean isPalindrome(String phrase) {
+        return true;
+    }
+
+}
--- src/test/java/com/developersdelicias/util/string/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/util/string/PalindromeAnalyzerTest.java
@@ -1,14 +1,23 @@
 package com.developersdelicias.util.string;
 
 import static org.junit.Assert.assertTrue;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Class to test a PalindromeAnalyzer implementation
+ *
  * @author Benjamin Cisneros <cisnerosbarraza@gmail.com>
  */
 public class PalindromeAnalyzerTest {
+
     PalindromeAnalyzer instance;
+
+    @Before
+    public void setUp() {
+        instance = new PalindromeAnalyzerImpl();
+    }
+
     @Test
     public void isPalindromeWithSinglePalindromeMustReturnTrue() {
         assertTrue(instance.isPalindrome("taco cat"));
