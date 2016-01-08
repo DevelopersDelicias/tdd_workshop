bf4b1cbf6f196568d768e5b11e08a1ea44413b52
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-07 19:30:30 -0600

    Add more tests for boolean values

--- src/main/java/com/developersdelicias/parser/BenjaminBooleanStringParser.java
+++ src/main/java/com/developersdelicias/parser/BenjaminBooleanStringParser.java
@@ -10,11 +10,24 @@ public class BenjaminBooleanStringParser implements BooleanStringParser {
     @Override
     public Boolean parse(String string) {
         Boolean result = null;
+
+        if (string == null) {
+            return null;
+        }
+
         switch (string) {
             case "yes":
+            case "y":
+            case "true":
+            case "t":
+            case "1":
                 result = Boolean.TRUE;
                 break;
             case "no":
+            case "n":
+            case "false":
+            case "f":
+            case "0":
                 result = Boolean.FALSE;
                 break;
         }
--- src/test/java/com/developersdelicias/parser/BooleanStringParserTest.java
+++ src/test/java/com/developersdelicias/parser/BooleanStringParserTest.java
@@ -14,15 +14,24 @@ public abstract class BooleanStringParserTest {
     @Test
     public void testParseTrueValues() {
         assertEquals(Boolean.TRUE, parser.parse("yes"));
+        assertEquals(Boolean.TRUE, parser.parse("y"));
+        assertEquals(Boolean.TRUE, parser.parse("true"));
+        assertEquals(Boolean.TRUE, parser.parse("t"));
+        assertEquals(Boolean.TRUE, parser.parse("1"));
     }
     
     @Test
     public void testParseFalseValues() {
         assertEquals(Boolean.FALSE, parser.parse("no"));
+        assertEquals(Boolean.FALSE, parser.parse("n"));
+        assertEquals(Boolean.FALSE, parser.parse("false"));
+        assertEquals(Boolean.FALSE, parser.parse("f"));
+        assertEquals(Boolean.FALSE, parser.parse("0"));
     }
     
     @Test
     public void testParseNullValues() {
         assertNull(parser.parse("anything else"));
+        assertNull(parser.parse(null));
     }
 }
