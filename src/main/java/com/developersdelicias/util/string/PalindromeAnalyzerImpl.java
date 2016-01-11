94352704ea4842a95a964d15c68cf9325f0dd2b6
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-11 14:44:24 -0600

    Refactor code using StringBuilder class

--- src/main/java/com/developersdelicias/util/string/PalindromeAnalyzerImpl.java
+++ src/main/java/com/developersdelicias/util/string/PalindromeAnalyzerImpl.java
@@ -26,24 +26,7 @@ public class PalindromeAnalyzerImpl implements PalindromeAnalyzer {
         if (phraseWithoutSpaces.isEmpty()) {
             return false;
         }
-        final int lengthWithoutSpaces = phraseWithoutSpaces.length();
-        String firstHalfPhrase = phraseWithoutSpaces.substring(0, lengthWithoutSpaces / 2);
-        String secondHalfPhrase = phraseWithoutSpaces.substring(lengthWithoutSpaces / 2);
-
-        boolean equals = true;
-        for (int i = 0; i < firstHalfPhrase.length(); i++) {
-            final char firstLetter = firstHalfPhrase.charAt(i);
-            final char lastLetter = secondHalfPhrase.charAt(secondHalfPhrase.length() - i - 1);
-
-            logger.debug("First Letter:" + firstLetter);
-            logger.debug("Last Letter:" + lastLetter);
-            if (firstLetter != lastLetter) {
-                equals = false;
-                logger.debug("Letters are not equal");
-                break;
-            }
-        }
-        return equals;
+        return phraseWithoutSpaces.equals(new StringBuilder(phraseWithoutSpaces).reverse().toString());
     }
 
 }
