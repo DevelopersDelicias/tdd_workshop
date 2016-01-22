8aebabb001984eac61143bed4e6a7c997314dabb
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-21 21:33:09 -0600

    Add custom BenjaminPalindromeAnalyzer implementation and its test class

---
+++ src/main/java/com/developersdelicias/palindrome/util/BenjaminPalindromeAnalyzer.java
@@ -0,0 +1,10 @@
+package com.developersdelicias.palindrome.util;
+
+public class BenjaminPalindromeAnalyzer implements PalindromeAnalyzer {
+
+	@Override
+	public boolean isPalindrome(String phrase) {
+		return false;
+	}
+
+}
