a21cbccbf3410ad299c0e578f08e154a8d847a40
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-22 14:12:13 -0600

    Refactor test method for using expected attribute of @Test annotation

--- src/test/java/com/developersdelicias/palindrome/util/PalindromeComplexityCalculatorTest.java
+++ src/test/java/com/developersdelicias/palindrome/util/PalindromeComplexityCalculatorTest.java
@@ -12,24 +12,20 @@ public abstract class PalindromeComplexityCalculatorTest {
 	public void withOnewWordPalindromeShouldReturnLowComplexity() {
 		assertSame(PalindromeComplexity.LOW, calculator.calculate("oso"));
 	}
-	
+
 	@Test
 	public void withFourWordsPalindromeShouldReturnMediumComplexity() throws Exception {
 		assertSame(PalindromeComplexity.MEDIUM, calculator.calculate("Anita lava la tina"));
 	}
-	
+
 	@Test
 	public void withSevenWordsPalindromeShouldReturnHighComplexity() throws Exception {
 		assertSame(PalindromeComplexity.HIGH, calculator.calculate("A man, a plan, a canal, Panama!"));
 	}
-	
-	@Test
+
+	@Test(expected=InvalidPalindromeException.class)
 	public void withInvalidPalindromeShouldThrowInvalidPalindromeException() throws Exception {
-		try {
-			calculator.calculate("not a palindrome");
-			fail("A InvalidPalindromeException is expected");
-		} catch (InvalidPalindromeException ipe) {
-		}
+		calculator.calculate("not a palindrome");
 	}
 
 }
