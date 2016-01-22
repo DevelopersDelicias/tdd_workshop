63acde19bb2ee6f235be82f6730f9bb4b69ba4f4
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-21 22:19:42 -0600

    Add test to validate palindrome that are questions

--- src/test/java/com/developersdelicias/palindrome/util/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/palindrome/util/PalindromeAnalyzerTest.java
@@ -5,32 +5,37 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public abstract class PalindromeAnalyzerTest {
-	
+
 	protected PalindromeAnalyzer analyzer;
 
 	@Test
 	public void withSinglePalindromeShouldReturnTrue() {
 		assertTrue(analyzer.isPalindrome("oso"));
 	}
-	
+
 	@Test
 	public void withNotRealPalindromeShouldReturnFalse() {
 		assertFalse(analyzer.isPalindrome("osos"));
 	}
-	
+
 	@Test
 	public void withPalindromeThatHaveSpacesShouldReturnTrue() throws Exception {
 		assertTrue(analyzer.isPalindrome("taco cat"));
 	}
-	
+
 	@Test
 	public void withPalindromeThatHaveUpperAndLowerCasesShouldReturnTrue() throws Exception {
 		assertTrue(analyzer.isPalindrome("Race car"));
 	}
-	
+
 	@Test
 	public void withPalindromeThatHaveCommaShouldReturnTrue() throws Exception {
 		assertTrue(analyzer.isPalindrome("Amor, Roma"));
+	}
+
+	@Test
+	public void withPalindromeThatIsAQuestionMustReturnTrue() throws Exception {
+		assertTrue(analyzer.isPalindrome("Was it a car or a cat I saw?"));
 	}
 
 }
