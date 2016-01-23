79e46ff9c8f11978ac02f80a8045bd933d568128
Benjamin Cisneros <cisnerosbarraza@gmail.com>
2016-01-23 12:23:50 -0600

    Test for code smells

--- src/main/java/com/developersdelicias/palindrome/util/BenjaminPalindromeAnalyzer.java
+++ src/main/java/com/developersdelicias/palindrome/util/BenjaminPalindromeAnalyzer.java
@@ -8,9 +8,19 @@ public class BenjaminPalindromeAnalyzer implements PalindromeAnalyzer {
 
 	@Override
 	public boolean isPalindrome(String phrase) {
+		
+		if (phrase == null || phrase.isEmpty()){
+			return false;
+		}
 		logger.debug("Phrase: " + phrase);
 		String phraseWithoutSymbols = phrase.replaceAll("[^a-zA-Z]", "");
+		
+		
 		logger.debug("No symbols: " + phraseWithoutSymbols);
+		if (phraseWithoutSymbols.isEmpty()) {
+			return false;
+		}
+		
 		String reversed = new StringBuilder(phraseWithoutSymbols).reverse().toString();
 		logger.debug("Reversed: " + reversed);
 		return phraseWithoutSymbols.equalsIgnoreCase(reversed);
--- src/test/java/com/developersdelicias/palindrome/util/PalindromeAnalyzerTest.java
+++ src/test/java/com/developersdelicias/palindrome/util/PalindromeAnalyzerTest.java
@@ -64,5 +64,20 @@ public abstract class PalindromeAnalyzerTest {
 	public void withAnySignShouldReturnTrue() throws Exception {
 		assertTrue(analyzer.isPalindrome("Are we not pure? “No sir!” Panama’s moody Noriega brags. “It is garbage!” Irony dooms a man; a prisoner up to new era."));
 	}
+	
+	@Test
+	public void withNullShouldReturnFalse() throws Exception {
+		assertFalse(analyzer.isPalindrome(null));
+	}
+	
+	@Test
+	public void withEmptyStringShouldReturnFalse() throws Exception {
+		assertFalse(analyzer.isPalindrome(""));
+	}
+	
+	@Test
+	public void withOnlySimbolsStringShouldReturnFalse() throws Exception {
+		assertFalse(analyzer.isPalindrome("!&=087%#"));
+	}
 
 }
